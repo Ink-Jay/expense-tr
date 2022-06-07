ad1066fad0c676cee9e3bbf3f199ae98f7448ab5
Ink-Jay <agnetta.khasamani@students.jkuat.ac.ke>
2022-06-07 16:16:13 +0300

    addTransactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,5 @@
+const Transaction = require('../models/Transaction');
+
 // Get all transactions
 // @ route: GET /ap1/v1/transactions
 // @ access: Public
@@ -5,22 +7,50 @@
 const req = require("express/lib/request");
 const res = require("express/lib/response");
 
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
+exports.getTransactions = async(req, res, next) => {
+    try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 // Add Transaction
 // @ route: POST /ap1/v1/transactions
 // @ access: Public
 
-exports.addTransaction = (req, res, next) => {
-    res.send('POST transaction')
+exports.addTransaction = async(req, res, next) => {
+
+    try {
+  
+     const { text, amount} = req.body;
+
+     const transaction = await Transaction.create(req.body);
+
+     return res.status(201).json({
+         success: true,
+         data: transaction
+     });
+        
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 // Delete Transaction
 // @ route: DELETE /ap1/v1/transactions:id
 // @ access: Public
 
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async(req, res, next) => {
     res.send('DELETE transaction')
 }
